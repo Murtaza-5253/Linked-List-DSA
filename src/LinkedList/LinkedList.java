b566f33e1a52070c1e131b86539d9a959b0c68ce
Murtaza-5253 <murtazamaimoon73@gmail.com>
2022-06-08 16:55:00 +0530

    Insert First in Linked List

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -4,10 +4,37 @@ public class LinkedList
 {
     private Link head;
     private Link tail;
+    int location;
 
     public LinkedList(){
         head=null;
         tail = null;
     }
 
+    public void insertFirst(int data){
+        Link link = new Link(data);
+        if(head==null){
+            head=link;
+            tail=link;
+        }
+        else{
+            link.next = head;
+            head = link;
+        }
+        location++;
+        System.out.println("Location:- "+location);
+    }
+
+    public void display(){
+        if(head==null){
+            System.out.println("List is Empty");
+        }
+        else{
+            Link temp = head;
+            while(temp!=null){
+                System.out.print(temp.data+" -> ");
+                temp = temp.next;
+            }
+        }
+    }
 }
--- src/LinkedList/LinkedListDemo.java
+++ src/LinkedList/LinkedListDemo.java
@@ -1,11 +1,15 @@
 package LinkedList;
 
-import java.util.Scanner;
-
 public class LinkedListDemo
 {
     public static void main(String[] args) {
         System.out.println(".........................Welcome............................");
         LinkedList list = new LinkedList();
+        list.insertFirst(15);
+        list.insertFirst(22);
+        list.insertFirst(35);
+
+        list.display();
+
     }
 }
